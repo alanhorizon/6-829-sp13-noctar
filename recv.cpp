fcb0c22a815fd9cee1f3dbd353fcd12eee4a3862
boknilev <belinkov@mit.edu>
2013-05-01 21:27:34 -0400

    initial commit

---
+++ recv.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+
+int main() {
+  //const std::string file_name = "/dev/langford";
+  int fd_read = open("/dev/langford", O_RDONLY);
+  int fd_write = open("/home/belinkov/classes/6.829/project/written", 
+                                O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR 
+                                  | S_IROTH | S_IWOTH );
+  char buf[32];
+  ssize_t num_read = read(fd_read, buf, 32);
+  close(fd_read);
+  
+  for (int i=0; i<32; i++) {
+    std::cout << buf[i];
+  }
+  write(fd_write, buf, num_read);
+  close(fd_write);
+  
+ 
+}
+
+    
+    
+    
+
+
+
